279748ae3676dbfbc1f55e7a01526378bfb97f90
mpgxvii <mpgxvii@gmail.com>
2020-01-28 17:12:50 +0000

    Update env vars to include auth details

--- authorizer-app/src/environments/environment.prod.ts
+++ authorizer-app/src/environments/environment.prod.ts
@@ -1,5 +1,13 @@
 export const environment = {
   production: true,
   BACKEND_BASE_URL: '/rest-sources/backend',
-  VALIDATE: true
+  VALIDATE: true,
+  AUTH: {
+    grant_type: 'authorization_code',
+    client_id: 'radar_rest_sources_auth',
+    client_secret: 'secret',
+    scope:
+      'SOURCETYPE.READ PROJECT.READ SOURCE.READ SUBJECT.READ MEASUREMENT.READ'
+  },
+  AUTH_URI: 'http://localhost:8080/oauth'
 };
--- authorizer-app/src/environments/environment.ts
+++ authorizer-app/src/environments/environment.ts
@@ -5,7 +5,15 @@
 export const environment = {
   production: false,
   BACKEND_BASE_URL: 'http://localhost:8080',
-  VALIDATE: false
+  VALIDATE: false,
+  AUTH: {
+    grant_type: 'authorization_code',
+    client_id: 'radar_rest_sources_auth',
+    client_secret: 'secret',
+    scope:
+      'SOURCETYPE.READ PROJECT.READ SOURCE.READ SUBJECT.READ MEASUREMENT.READ'
+  },
+  AUTH_URI: 'http://localhost:8080/oauth'
 };
 
 /*
