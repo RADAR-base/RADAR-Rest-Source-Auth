9d512af3119efb675830d9c82b9b25248f9c38f2
mpgxvii <mpgxvii@gmail.com>
2020-01-29 15:05:31 +0000

    Fix tests

--- authorizer-app/src/app/services/management-portal-auth.service.spec.ts
+++ authorizer-app/src/app/services/management-portal-auth.service.spec.ts
@@ -1,21 +1,25 @@
-import { HttpClientModule } from "@angular/common/http";
+import { HttpClient, HttpHandler } from "@angular/common/http";
+
 import { JwtHelperService } from "@auth0/angular-jwt";
-import { RestSourceUserService } from "./rest-source-user.service";
+import { ManagementPortalAuthService } from "./management-portal-auth.service";
 import { TestBed } from "@angular/core/testing";
 
-describe("DeviceAuthorizationService", () => {
+describe("ManagementPortalAuthService", () => {
   beforeEach(() =>
     TestBed.configureTestingModule({
-      imports: [HttpClientModule],
       providers: [
-        RestSourceUserService,
+        ManagementPortalAuthService,
+        HttpClient,
+        HttpHandler,
         { provide: JwtHelperService, useClass: JwtHelperServiceMock }
       ]
     })
   );
 
   it("should be created", () => {
-    const service: RestSourceUserService = TestBed.get(RestSourceUserService);
+    const service: ManagementPortalAuthService = TestBed.get(
+      ManagementPortalAuthService
+    );
     expect(service).toBeTruthy();
   });
 });
