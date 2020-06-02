f929e7026120816f35996af54a55224bbcd47fb9
mpgxvii <mpgxvii@gmail.com>
2020-06-02 20:09:46 +0100

    make changes to sed in entrypoint script

--- authorizer-app/docker/entrypoint.sh
+++ authorizer-app/docker/entrypoint.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 
 ROOT_DIR=/usr/share/nginx/html
-
+QUOTE='"'
 # Replace env vars in JavaScript files
 echo "Replacing env vars in JS"
 for file in $ROOT_DIR/main.*.js* $ROOT_DIR/index.html $ROOT_DIR/polyfills.*.js* $ROOT_DIR/runtime.*.js*
 do
   echo "Processing $file ...";
 
-  sed -i 's|/\?BASE_HREF|'${BASE_HREF}'|g' $file
-  sed -i 's|BACKEND_BASE_URL|'${BACKEND_BASE_URL}'|g' $file
-  sed -i 's|VALIDATE|'${VALIDATE}'|g' $file
-  sed -i 's|AUTH_GRANT_TYPE|'${AUTH_GRANT_TYPE}'|g' $file
-  sed -i 's|AUTH_CLIENT_ID|'${AUTH_CLIENT_ID}'|g' $file
-  sed -i 's|AUTH_CLIENT_SECRET|'${AUTH_CLIENT_SECRET}'|g' $file
-  sed -i 's|AUTH_CALLBACK_URL|'${AUTH_CALLBACK_URL}'|g' $file
-  sed -i 's|AUTH_URI|'${AUTH_URI}'|g' $file
+  sed -i 's|"BASE_HREF"|'${QUOTE}${BASE_HREF}${QUOTE}'|g' $file
+  sed -i 's|"BACKEND_BASE_URL"|'${QUOTE}${BACKEND_BASE_URL}${QUOTE}'|g' $file
+  sed -i 's|"VALIDATE"|'${QUOTE}${VALIDATE}${QUOTE}'|g' $file
+  sed -i 's|"AUTH_GRANT_TYPE"|'${QUOTE}${AUTH_GRANT_TYPE}${QUOTE}'|g' $file
+  sed -i 's|"AUTH_CLIENT_ID"|'${QUOTE}${AUTH_CLIENT_ID}${QUOTE}'|g' $file
+  sed -i 's|"AUTH_CLIENT_SECRET"|'${QUOTE}${AUTH_CLIENT_SECRET}${QUOTE}'|g' $file
+  sed -i 's|"AUTH_CALLBACK_URL"|'${QUOTE}${AUTH_CALLBACK_URL}${QUOTE}'|g' $file
+  sed -i 's|"AUTH_URI"|'${QUOTE}${AUTH_URI}${QUOTE}'|g' $file
 done
 
 echo "Static files ready"
