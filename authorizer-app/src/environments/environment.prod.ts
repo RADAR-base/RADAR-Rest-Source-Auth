c0313345ac9f2797671d4cfe38f176a65643975e
nivethika <nivemaham@gmail.com>
2020-06-02 15:41:10 +0200

    correct prod profile variable values

--- authorizer-app/src/environments/environment.prod.ts
+++ authorizer-app/src/environments/environment.prod.ts
@@ -1,19 +1,19 @@
 export const environment = {
   production: true,
   // base url of the rest-sources authorizer app
-  BASE_HREF: '/rest-sources/authorizer/',
+  BASE_HREF: 'BASE_HREF',
   // base url of the rest-sources-auth-backend
-  BACKEND_BASE_URL: 'http://localhost:8080',
+  BACKEND_BASE_URL: 'BACKEND_BASE_URL',
   // If user validation is enabled
-  VALIDATE: false,
+  VALIDATE: 'VALIDATE',
   // Grant-type of authorization
-  AUTH_GRANT_TYPE: 'authorization_code',
+  AUTH_GRANT_TYPE: 'AUTH_GRANT_TYPE',
   // Client id of the authorizer app.
-  AUTH_CLIENT_ID: 'radar_rest_sources_auth',
+  AUTH_CLIENT_ID: 'AUTH_CLIENT_ID',
   // Client secret of the authorizer app.
-  AUTH_CLIENT_SECRET: 'secret',
+  AUTH_CLIENT_SECRET: 'AUTH_CLIENT_SECRET',
   // Callback URL registered in MP.
-  AUTH_CALLBACK_URL: 'http://localhost:8080/oauth/callback',
+  AUTH_CALLBACK_URL: 'AUTH_CALLBACK_URL',
   // Management Portal URL.
-  AUTH_URI: 'http://localhost:8080/oauth'
+  AUTH_URI: 'AUTH_URI'
 };
