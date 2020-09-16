0c5e7ceb0a2f60e0f19597852a10d639fe1f0d8e
nivethika <nivemaham@gmail.com>
2020-09-16 18:08:50 +0200

    prompt error messages from server side to user.

--- authorizer-app/src/app/services/rest-source-user.service.ts
+++ authorizer-app/src/app/services/rest-source-user.service.ts
@@ -1,6 +1,6 @@
 import {Injectable} from '@angular/core';
 import {HttpClient, HttpParams} from '@angular/common/http';
-import {Observable} from 'rxjs/internal/Observable';
+import {Observable} from 'rxjs';
 import {RestSourceUser} from '../models/rest-source-user.model';
 import {environment} from '../../environments/environment';
 import {RadarProject} from "../models/rest-source-project.model";
