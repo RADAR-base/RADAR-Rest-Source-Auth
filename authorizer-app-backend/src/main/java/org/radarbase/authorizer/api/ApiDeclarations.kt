506c502490ea9359cd4a9098944eaaa79927a12a
mpgxvii <mpgxvii@gmail.com>
2021-02-08 22:20:47 +0800

    Add DeregistrationParams data class

--- authorizer-app-backend/src/main/java/org/radarbase/authorizer/api/ApiDeclarations.kt
+++ authorizer-app-backend/src/main/java/org/radarbase/authorizer/api/ApiDeclarations.kt
@@ -47,6 +47,11 @@ data class SignRequestParams(
     val parameters: Map<String, String?>,
 )
 
+data class DeregistrationParams(
+    val userId: String,
+    val userAccessToken: String
+)
+
 data class RequestTokenPayload(
     var sourceType: String,
     var code: String? = null,
