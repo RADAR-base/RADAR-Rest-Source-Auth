0c46006a56dee6e079f539a217bd0fb0c31b0362
Joris Borgdorff <joris@thehyve.nl>
2021-08-30 14:48:30 +0200

    Fix ktlint issues

--- authorizer-app-backend/src/main/java/org/radarbase/authorizer/api/ApiDeclarations.kt
+++ authorizer-app-backend/src/main/java/org/radarbase/authorizer/api/ApiDeclarations.kt
@@ -18,7 +18,6 @@ package org.radarbase.authorizer.api
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonProperty
-import java.io.Serializable
 import java.time.Instant
 
 @JsonIgnoreProperties(ignoreUnknown = true)
--- authorizer-app-backend/src/test/java/org/radarbase/authorizer/AuthorizerServiceConfigTest.kt
+++ authorizer-app-backend/src/test/java/org/radarbase/authorizer/AuthorizerServiceConfigTest.kt
@@ -4,14 +4,12 @@ import okhttp3.HttpUrl.Companion.toHttpUrl
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.Matchers.equalTo
 import org.junit.jupiter.api.Test
-
 import java.net.URI
 
 internal class AuthorizerServiceConfigTest {
 
     @Test
     fun getCallbackUrl() {
-
         val config = AuthorizerServiceConfig(
             advertisedBaseUri = URI("http://something/"),
         )
@@ -24,17 +22,14 @@ internal class AuthorizerServiceConfigTest {
         val config = AuthorizerServiceConfig(
             advertisedBaseUri = URI("http://something//backend//"),
         )
-
         assertThat(config.callbackUrl, equalTo("http://something/authorizer/users:new".toHttpUrl()))
     }
-
 
     @Test
     fun getFrontendCallbackUrl() {
         val config = AuthorizerServiceConfig(
             frontendBaseUri = URI("http://something/authorizer"),
         )
-
         assertThat(config.callbackUrl, equalTo("http://something/authorizer/users:new".toHttpUrl()))
     }
 }
