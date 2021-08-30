100dcdc6820d7f795421d27660529bab020fe519
Joris Borgdorff <joris@thehyve.nl>
2021-08-30 14:34:14 +0200

    More complete test

--- authorizer-app-backend/src/test/java/org/radarbase/authorizer/AuthorizerServiceConfigTest.kt
+++ authorizer-app-backend/src/test/java/org/radarbase/authorizer/AuthorizerServiceConfigTest.kt
@@ -1,12 +1,10 @@
 package org.radarbase.authorizer
 
-import okhttp3.HttpUrl
 import okhttp3.HttpUrl.Companion.toHttpUrl
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.Matchers.equalTo
 import org.junit.jupiter.api.Test
 
-import org.junit.jupiter.api.Assertions.*
 import java.net.URI
 
 internal class AuthorizerServiceConfigTest {
@@ -16,6 +14,25 @@ internal class AuthorizerServiceConfigTest {
 
         val config = AuthorizerServiceConfig(
             advertisedBaseUri = URI("http://something/"),
+        )
+
+        assertThat(config.callbackUrl, equalTo("http://something/authorizer/users:new".toHttpUrl()))
+    }
+
+    @Test
+    fun getEmptyCallbackUrl() {
+        val config = AuthorizerServiceConfig(
+            advertisedBaseUri = URI("http://something//backend//"),
+        )
+
+        assertThat(config.callbackUrl, equalTo("http://something/authorizer/users:new".toHttpUrl()))
+    }
+
+
+    @Test
+    fun getFrontendCallbackUrl() {
+        val config = AuthorizerServiceConfig(
+            frontendBaseUri = URI("http://something/authorizer"),
         )
 
         assertThat(config.callbackUrl, equalTo("http://something/authorizer/users:new".toHttpUrl()))
