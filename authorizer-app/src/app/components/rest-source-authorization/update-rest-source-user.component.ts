b26118e23d56403a72b1ca8ac92764bc53c7b8b5
Peyman Mohtashami <p.mohtashami@gmail.com>
2021-09-01 15:29:39 +0200

    Fix link url and remove extra forward slash

--- authorizer-app/src/app/components/rest-source-authorization/update-rest-source-user.component.ts
+++ authorizer-app/src/app/components/rest-source-authorization/update-rest-source-user.component.ts
@@ -259,7 +259,7 @@ export class UpdateRestSourceUserComponent implements OnInit {
         } else if (registrationResp.secret) {
           // generate link // show // copy to clipboard
           this.linkForUser =
-            `${environment.baseUrl}/users:auth?token=${registrationResp.token}&secret=${registrationResp.secret}`;
+            `${environment.radarBaseUrl}${environment.BASE_HREF}/users:auth?token=${registrationResp.token}&secret=${registrationResp.secret}`;
           this.linkGeneratingLoading = false;
         }
       },
