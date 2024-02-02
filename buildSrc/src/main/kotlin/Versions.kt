25f22bd7ed8eeac0328046d833ad512b36c4079c
Bastiaan <bastiaan@thehyve.nl>
2024-02-02 10:16:43 +0100

    use radar-jersey snapshot

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -7,7 +7,7 @@ object Versions {
     const val kotlin = "1.9.10"
 
     const val radarCommons = "1.1.1"
-    const val radarJersey = "0.11.0"
+    const val radarJersey = "0.11.0-SNAPSHOT"
     const val postgresql = "42.6.0"
     const val ktor = "2.3.5"
     const val jedis = "5.0.2"
