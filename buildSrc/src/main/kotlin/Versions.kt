8907cc60a7775fe80a477ff6e0e5ae7eea411c7b
Bastiaan <bastiaan@thehyve.nl>
2024-06-05 17:39:36 +0200

    Security updates

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -4,20 +4,20 @@ object Versions {
 
     const val java = 17
 
-    const val kotlin = "1.9.10"
+    const val kotlin = "1.9.23"
 
-    const val radarCommons = "1.1.1"
+    const val radarCommons = "1.1.2"
     const val radarJersey = "0.11.1"
-    const val postgresql = "42.6.0"
-    const val ktor = "2.3.5"
-    const val jedis = "5.0.2"
+    const val postgresql = "42.6.1"
+    const val ktor = "2.3.11"
+    const val jedis = "5.1.3"
 
-    const val slf4j = "2.0.9"
-    const val log4j2 = "2.21.0"
+    const val slf4j = "2.0.13"
+    const val log4j2 = "2.23.1"
 
-    const val jersey = "3.1.3"
-    const val junit = "5.10.0"
-    const val mockitoKotlin = "5.1.0"
+    const val jersey = "3.1.7"
+    const val junit = "5.10.2"
+    const val mockitoKotlin = "5.3.1"
     const val hamcrest = "2.2"
 
     const val gradle = "8.4"
