3b6aa9f65de2bac8807b300761ee3f11a2c31768
pvannierop <pim@thehyve.nl>
2024-10-04 13:32:42 +0200

    Add support for Sentry monitoring
    Includes addition to documentation.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,28 +1,10 @@
-import org.radarbase.gradle.plugin.radarKotlin
-
 plugins {
     id("org.radarbase.radar-root-project") version Versions.radarCommons
     id("org.radarbase.radar-dependency-management") version Versions.radarCommons
     id("org.radarbase.radar-kotlin") version Versions.radarCommons apply false
-    kotlin("plugin.serialization") version Versions.kotlin apply false
-    kotlin("plugin.noarg") version Versions.kotlin apply false
-    kotlin("plugin.jpa") version Versions.kotlin apply false
-    kotlin("plugin.allopen") version Versions.kotlin apply false
 }
 
 radarRootProject {
     projectVersion.set(Versions.project)
     gradleVersion.set(Versions.gradle)
-}
-
-subprojects {
-    apply(plugin = "org.radarbase.radar-kotlin")
-
-    radarKotlin {
-        javaVersion.set(Versions.java)
-        kotlinVersion.set(Versions.kotlin)
-        slf4jVersion.set(Versions.slf4j)
-        log4j2Version.set(Versions.log4j2)
-        junitVersion.set(Versions.junit)
-    }
 }
