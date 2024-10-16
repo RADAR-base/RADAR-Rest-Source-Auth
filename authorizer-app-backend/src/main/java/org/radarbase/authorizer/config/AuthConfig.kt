f773bd6da95bedc21116b47b0381bd72e17dac86
Pauline <mpgxvii@gmail.com>
2024-10-16 12:55:05 +0800

    Update default config

--- authorizer-app-backend/src/main/java/org/radarbase/authorizer/config/AuthConfig.kt
+++ authorizer-app-backend/src/main/java/org/radarbase/authorizer/config/AuthConfig.kt
@@ -2,12 +2,12 @@ package org.radarbase.authorizer.config
 
 data class AuthConfig(
     val managementPortalUrl: String = "http://managementportal-app:8080/managementportal",
-    val authUrl: String = "",
+    val authUrl: String = "http://hydra-public:4444/oauth2/token",
     val clientId: String = "radar_rest_sources_auth_backend",
     val clientSecret: String? = "",
     val jwtECPublicKeys: List<String>? = null,
     val jwtRSAPublicKeys: List<String>? = null,
     val jwtIssuer: String? = null,
     val jwtResourceName: String = "res_restAuthorizer",
-    val jwksUrls: List<String> = listOf("http://host.docker.internal:4445/admin/keys/hydra.jwt.access-token"),
+    val jwksUrls: List<String> = listOf("http://hydra-admin:4445/admin/keys/hydra.jwt.access-token"),
 )
