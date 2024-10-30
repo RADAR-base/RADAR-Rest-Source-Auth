86c3a2d9143f4de22ae9ad10a33a7c45ce939a56
pvannierop <pim@thehyve.nl>
2024-10-30 17:43:55 +0100

    Up radar-jersey version to 0.12.0
    Fixes a bug with project names with spaces.

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -7,7 +7,7 @@ object Versions {
     const val kotlin = "1.9.23"
 
     const val radarCommons = "1.1.2"
-    const val radarJersey = "0.11.2"
+    const val radarJersey = "0.12.0"
     const val postgresql = "42.6.1"
     const val ktor = "2.3.11"
     const val jedis = "5.1.3"
