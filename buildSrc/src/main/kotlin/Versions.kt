205ef0679262a4f9c2818b279bfc3f5262f56377
EIjo <eljo@live.nl>
2025-06-24 14:59:15 +0200

    Up version of radarCommons to 1.2.2

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,12 +1,12 @@
 @Suppress("ConstPropertyName")
 object Versions {
-    const val project = "4.4.9"
+    const val project = "4.5.0"
 
     const val java = 17
 
     const val kotlin = "1.9.23"
 
-    const val radarCommons = "1.1.3"
+    const val radarCommons = "1.2.2"
     const val radarJersey = "0.12.2"
     const val postgresql = "42.6.1"
     const val ktor = "2.3.11"
