0fb87ca4470b9cbc446d03307892f7a2c019d6d9
Pim van Nierop <pim@thehyve.nl>
2025-10-23 11:57:07 +0200

    fix: up radar-jersey to 0.12.5

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -7,7 +7,7 @@ object Versions {
     const val kotlin = "1.9.23"
 
     const val radarCommons = "1.2.4"
-    const val radarJersey = "0.12.4"
+    const val radarJersey = "0.12.5"
     const val postgresql = "42.6.1"
     const val ktor = "2.3.11"
     const val jedis = "5.1.3"
